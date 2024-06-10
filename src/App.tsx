bf6ceb1575f5fa42f319d9216e7836c75f564939
Gabriel Evangelista <gabriel.evanger@gmail.com>
2024-06-10 15:32:08 -0400

    feat: adicionando botão

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { Button } from './components/ui/button'
+import './global.css'
 export function App() {
   return (
- <h1>hello world</h1>
+    <div className="flex justify-center items-center h-screen">
+        <Button>Enviar</Button>
+    </div>
   )
 }
 
