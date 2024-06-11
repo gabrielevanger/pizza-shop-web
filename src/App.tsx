a504e84edf9078a32192064364d4f7ce2c39e53d
Gabriel Evangelista <gabriel.evanger@gmail.com>
2024-06-11 09:53:30 -0400

    chore: aplicando correção semântica.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import "./global.css"
+import './global.css'
 
-import { Button } from "./components/ui/button"
+import { Button } from './components/ui/button'
 export function App() {
   return (
     <div className="flex h-screen items-center justify-center">
