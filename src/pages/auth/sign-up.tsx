5f68f07c6ce6f5369c09e14bad8d233e21c07e12
Gabriel Evangelista <gabriel.evanger@gmail.com>
2024-06-17 11:09:44 -0400

    feat: Layout do app com cabeçalho

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -28,7 +28,6 @@ export function SignUp() {
 
   async function handleSignUp(data: SignUpForm) {
     try {
-      console.log(data)
       await new Promise((resolve) => setTimeout(resolve, 2000))
 
       toast.success('Restaurante cadastrado com sucesso!', {
