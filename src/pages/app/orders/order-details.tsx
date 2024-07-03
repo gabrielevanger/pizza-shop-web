1257ca7618e857932fddb07e7d537effe8f0e4b7
Gabriel Evangelista <gabriel.evanger@gmail.com>
2024-07-03 18:58:20 -0400

    feat: Componente: Detalhes do pedido

---
+++ src/pages/app/orders/order-details.tsx
@@ -0,0 +1,109 @@
+import { DialogTitle } from '@radix-ui/react-dialog'
+
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+} from '@/components/ui/dialog'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+// export interface OrderDetailsProps {}
+export function OrderDetails() {
+  return (
+    <div>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Pedido:1827aa8sdqwe</DialogTitle>
+          <DialogDescription>Detalhes do pedido</DialogDescription>
+        </DialogHeader>
+
+        <div className="space-y-6">
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell className="text-muted-foreground">Status</TableCell>
+                <TableCell className="flex justify-end">
+                  <div className="flex items-center gap-2">
+                    <span className="h-2 w-2 rounded-full bg-slate-400" />
+                    <span className="font-medium text-muted-foreground">
+                      Pendentes
+                    </span>
+                  </div>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-muted-foreground">Cliente</TableCell>
+                <TableCell className="flex justify-end">
+                  Gabriel de Oliveira Evangelista
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell className="text-muted-foreground">
+                  Telefone
+                </TableCell>
+                <TableCell className="flex justify-end">
+                  (95) 99999-9999
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-muted-foreground">E-mail</TableCell>
+                <TableCell className="flex justify-end">
+                  gabriel.evanger@gmail.com
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell className="text-muted-foreground">
+                  Realizado há
+                </TableCell>
+                <TableCell className="flex justify-end">há 3 minutos</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Produto</TableHead>
+                <TableHead className="text-right">Qtd.</TableHead>
+                <TableHead className="text-right">Preço</TableHead>
+                <TableHead className="text-right">Subtotal</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>Pizza Calabresa familia</TableCell>
+                <TableCell className="text-right">2</TableCell>
+                <TableCell className="text-right">R$ 69,90</TableCell>
+                <TableCell className="text-right">R$ 139,80</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Pizza Bacon familia</TableCell>
+                <TableCell className="text-right">2</TableCell>
+                <TableCell className="text-right">R$ 59,90</TableCell>
+                <TableCell className="text-right">R$ 119,80</TableCell>
+              </TableRow>
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan={3}>Total do pedido</TableCell>
+                <TableCell className="text-right font-medium">
+                  R$ 279,60
+                </TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </div>
+      </DialogContent>
+    </div>
+  )
+}
--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,16 +1,25 @@
+import { Dialog } from '@radix-ui/react-dialog'
 import { ArrowRight, Search, X } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
+import { DialogTrigger } from '@/components/ui/dialog'
 import { TableCell, TableRow } from '@/components/ui/table'
+
+import { OrderDetails } from './order-details'
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline" size="xs">
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+          <OrderDetails />
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
         aasd585asd85
